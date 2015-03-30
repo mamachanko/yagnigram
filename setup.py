7ac59842dc7a4ca2fa609187bf90e0462a65522e
mamachanko <max@rootswiseyouths.com>
2015-03-30 19:57:13 +0200

    clean requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,22 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    # TODO: put package requirements here
+    'binaryornot==0.3.0',
+    'blessings==1.6',
+    'click==3.3',
+    'docopt==0.6.2',
+    'falcon==0.2',
+    'httplib2==0.9',
+    'Jinja2==2.7.3',
+    'MarkupSafe==0.23',
+    'mock==1.0.1',
+    'Pillow==2.7.0',
+    'python-instagram==1.3.0',
+    'python-mimeparse==0.1.4',
+    'PyYAML==3.11',
+    'requests==2.5.3',
+    'simplejson==3.6.5',
+    'six==1.9.0',
 ]
 
 test_requirements = [
